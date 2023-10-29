4a23012b1b34d552060183c8b696057f092860a3
surajitr1 <surajitr1@vmware.com>
2023-10-29 20:58:50 +0530

    feat[console-utils]: added more console utility

--- utils/console-utils.js
+++ utils/console-utils.js
@@ -18,4 +18,28 @@ const write = function (message) {
   }
 };
 
-module.exports = { write };
+const logNote = function (message, newline) {
+  const formattedMessage = newline ? `\n◕‿◕ ${message}\n` : `◕‿◕ ${message}`;
+  console.log(formattedMessage);
+};
+
+const logConsole = function (message, newline) {
+  const formattedMessage = newline ? `\n˃ ${message}\n` : `˃ ${message}`;
+  console.log(formattedMessage);
+};
+
+const logExeBlock = function (newline = true, message = null) {
+  message = message ? message.trim() : null;
+  const star = "=";
+  const offset = message && message.length % 2 === 0 ? 1 : 0;
+  const size = 100;
+  let logger = message
+    ? `${star.repeat(
+        (size - message.length) / 2 - 2
+      )}   ${message}  ${star.repeat((size - message.length) / 2 - 2 - offset)}`
+    : star.repeat(size);
+  logger = newline ? `\n${logger}\n` : logger;
+  console.log(logger);
+};
+
+module.exports = { write, logNote, logConsole, logExeBlock };
